18ad72805851e0fd4e686e716c1f56cff75f0622
Rodrigo Aluizio <r.aluizio@gmail.com>
2024-09-18 11:06:47 -0300

    more constants

--- src/constants.py
+++ src/constants.py
@@ -4,5 +4,7 @@ Constants
 
 JSON_LINK: str = "https://dl.secondarymetabolites.org/mibig/mibig_json_3.1.tar.gz"
 GBK_LINK: str = "https://dl.secondarymetabolites.org/mibig/mibig_gbk_3.1.tar.gz"
+FASTA_LINK: str = "https://dl.secondarymetabolites.org/mibig/mibig_prot_seqs_3.1.fasta"
 METADATA: str = "mibig_json_3.1.tar.gz"
 DATABASE: str = "mibig_gbk_3.1.tar.gz"
+PROTEIN: str = "mibig_prot_seqs_3.1.fasta"
