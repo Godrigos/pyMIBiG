6068e8955e05f1ad8c78292b10f6fac89c9cbf14
Rodrigo Aluizio <r.aluizio@gmail.com>
2024-10-02 13:18:49 -0300

    better filenames

--- src/pymibig/create_filename_prefix.py
+++ src/pymibig/create_filename_prefix.py
@@ -6,10 +6,13 @@ def create_filename_prefix(args) -> str:
     '''
     Take user arguments and compose filenames prefix with them.
     '''
-    return (
-        f'{args.organism if args.organism else ""}_'
-        f'{args.product if args.product else ""}_'
-        f'{args.biosynt if args.biosynt else ""}_'
-        f'{args.completeness}_'
-        f'{"minimal" if args.minimal else ""}'
-    )
+
+    org = f'{args.organism}_' if args.organism else ""
+    prod = f'{args.product}_' if args.product else ""
+    bio = f'{args.biosynt}_' if args.biosynt else ""
+    comp = f'{args.completeness}'
+    mini = f'{"_minimal" if args.minimal else ""}'
+
+    prefix = org + prod + bio + comp + mini
+
+    return prefix
